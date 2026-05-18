b4198c86987d00bab264fe3f9f517379e995a026
majo_project <Umarfarukhpakali1234@gmail.com>
2026-05-18 16:01:53 +0530

    Fix API base path fallback routing

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import streamlit as st
 # =============================
 # CONFIG
 # =============================
-API_BASE = "https://cinevault-movie-recommention-1.onrender.com/" or "https://movie-rec-466x.onrender.com" or "http://127.0.0.1:8000"
+API_BASE = "https://cinevault-movie-recommention-1.onrender.com/" or "https://movie-rec-466x.onrender.com"
 TMDB_IMG = "https://image.tmdb.org/t/p/w500"
 
 st.set_page_config(page_title="CineVault", page_icon="🎬", layout="wide")
